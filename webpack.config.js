a531e0705079fbb42f38d493563bfb0879e91b0f
bluewaitor <405622394@qq.com>
2017-12-13 15:24:12 +0800

    autoprefixer modify

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         ],
     },
     postcss: [
-        require('autoprefixer')({ browsers: ['last 5 versions'] })
+        require('autoprefixer')({ browsers: ['last 10 versions'] })
     ],
     plugins: [
         new ExtractTextPlugin("index.css")
